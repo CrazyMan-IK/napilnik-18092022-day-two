a1654d78cd12c3527e658fe65b3805bdaf591de7
Даниэль Давлетшин <Superdanik2004@gmail.com>
2022-09-18 14:26:55 +0500

    Added interact key

--- Assets/Source/Features/CharacterMovement/Code/Interfaces/IReadOnlyInput.cs
+++ Assets/Source/Features/CharacterMovement/Code/Interfaces/IReadOnlyInput.cs
@@ -1,9 +1,12 @@
+using System;
 using UnityEngine;
 
 namespace CharacterMovement.Interfaces
 {
     public interface IReadOnlyInput
     {
+        event Action InteractKeyPressed;
+
         Transform transform { get; }
         Vector2 Direction { get; }
         Vector2 MouseDelta { get; }
